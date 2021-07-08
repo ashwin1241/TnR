0d03b41b014e2420718c91b165b4a5050328f5aa
ashwin1241 <ashwinwadatkar@gmail.com>
2021-07-09 00:20:55 +0530

    Splashscreen done

--- app/src/main/java/com/tnr/MainActivity.java
+++ app/src/main/java/com/tnr/MainActivity.java
@@ -2,13 +2,45 @@ package com.tnr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH_TIMEOUT = 1000;
+    private Animation appear;
+    private ImageView logo;
+    private TextView wmrka;
+    private TextView wmrkb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logo = findViewById(R.id.logo_splash);
+        wmrka = findViewById(R.id.wmrka);
+        wmrkb = findViewById(R.id.wmrkb);
+
+        appear = AnimationUtils.loadAnimation(this,R.anim.appear_animation);
+
+        logo.setAnimation(appear);
+        wmrka.setAnimation(appear);
+        wmrkb.setAnimation(appear);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIMEOUT);
+
     }
 }
